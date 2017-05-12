666de53349eecdc55edbe066fbd57f90ca2406b7
Ewing Tsai <191237878@qq.com>
2017-05-12 15:44:51 +0800

    大幅提高ID生成性能

--- src/main/java/com/ewing/dandelion/GlobalIdWorker.java
+++ src/main/java/com/ewing/dandelion/GlobalIdWorker.java
@@ -5,30 +5,32 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
-import java.security.SecureRandom;
 
 /**
  * 可独立运行的全局ID生成器，保持趋势递增，线程安全，尾数0至9随机分布。
- * 41位(自动扩展位数)毫秒+1标志+48Mac地址+1标志+13位累加随机数。
- * 理想情况平均每秒可生成1638400个，实测生成百万个用时约2秒(视配置而定)。
- * 使用32位10进制存储可以使用到2300年之后，可以扩展长度。
+ * 41位(自动扩展位数)毫秒+48Mac地址+13位累加随机数。
+ * 理想情况平均每秒可生成8190950个，实测生成百万个用时不到1秒(视配置而定)。
+ * 使用32位10进制可以使用到3300年之后，使用21位36进制可使用到8500年以后，可以扩展长度。
  *
  * @author Ewing
  */
 public class GlobalIdWorker {
     // Mac地址掩码（48个1）
     private final static long macAddressMask = ~(-1L << 48L);
-    // Mac标志位 保证长度一定是48+1位
+    // Mac标志位 保证长度一定是48+1位 再用substring去掉标志位
     private final static long macAddressFlag = 1L << 48L;
 
     // 序号掩码（13个1）也是最大值
     private final static long sequenceMask = ~(-1L << 13L);
-    // 序号标志位 保证长度一定是13+1位
+    // 序号标志位 保证长度一定是13+1位 再用substring去掉标志位
     private final static long sequenceFlag = 1L << 13L;
 
     private static long lastTimestamp = System.currentTimeMillis();
 
     private static long sequence = 0L;
+
+    // 尾数离散度 可以分散ID的尾数分布
+    private static long discreteRate = 100L;
 
     private static String macAddressBit;
 
@@ -38,7 +40,7 @@ public class GlobalIdWorker {
     static {
         try {
             long macAddress = macAddressLong();
-            macAddressBit = Long.toBinaryString((macAddress & macAddressMask) | macAddressFlag);
+            macAddressBit = Long.toBinaryString((macAddress & macAddressMask) | macAddressFlag).substring(1);
         } catch (IOException e) {
             throw new RuntimeException("Init mac address fail.", e);
         }
@@ -69,20 +71,20 @@ public class GlobalIdWorker {
 
         if (lastTimestamp == timestamp) {
             // 当前毫秒内，则随机增加，避免尾数太集中
-            sequence = sequence + new SecureRandom().nextInt(10) + 1;
+            sequence = sequence + 1;
             if (sequence > sequenceMask) {
                 // 当前毫秒内计数满了，则等待下一毫秒
                 timestamp = tilNextMillis(lastTimestamp);
             }
         } else {
-            sequence = 0;
+            sequence = sequence % discreteRate;
         }
 
         lastTimestamp = timestamp;
 
         // ID偏移组合生成最终的ID，并返回ID
         String idBit = Long.toBinaryString(timestamp) + macAddressBit +
-                Long.toBinaryString(sequence | sequenceFlag);
+                Long.toBinaryString(sequence | sequenceFlag).substring(1);
 
         return new BigInteger(idBit, 2);
     }
