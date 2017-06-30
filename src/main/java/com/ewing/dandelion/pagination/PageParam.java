ca9efbaa9a9e0f73e615c66d5a2a2a75510417c7
Ewing Tsai <191237878@qq.com>
2017-07-01 01:34:25 +0800

    优化分页参数

--- src/main/java/com/ewing/dandelion/pagination/PageParam.java
+++ src/main/java/com/ewing/dandelion/pagination/PageParam.java
@@ -29,26 +29,23 @@ public class PageParam {
         return offset;
     }
 
-    public PageParam setOffset(int offset) {
+    public void setOffset(int offset) {
         this.offset = offset;
-        return this;
     }
 
     public int getLimit() {
         return limit;
     }
 
-    public PageParam setLimit(int limit) {
+    public void setLimit(int limit) {
         this.limit = limit;
-        return this;
     }
 
     public boolean isCount() {
         return count;
     }
 
-    public PageParam setCount(boolean count) {
+    public void setCount(boolean count) {
         this.count = count;
-        return this;
     }
 }
